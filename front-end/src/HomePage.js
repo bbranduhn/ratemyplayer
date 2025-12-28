4811b138ba4cc86382006bd32f4bb269d5cc9bd4
bbranduhn <br2412@nyu.edu>
2025-12-27 23:45:44 -0500

    setting up routes + homepage

---
+++ front-end/src/HomePage.js
@@ -0,0 +1,12 @@
+import logo from './logo-transp.png';
+import React, { useState } from 'react';
+import './HomePage.css';
+
+export function HomePage() {
+  return (
+    <div className="HomePage">
+        <img src={logo} className="App-logo" alt="logo" />
+    </div>
+  );
+}
+
